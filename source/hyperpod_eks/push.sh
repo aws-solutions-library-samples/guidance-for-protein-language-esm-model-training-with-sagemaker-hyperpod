a3246d110020603de0d89bb626e198e2f5c6ef57
Daniel Zilberman <42846514+dzilbermanvmw@users.noreply.github.com>
2025-04-08 16:21:58 -0700

    Update push.sh
    updated ECR repo URL

--- source/hyperpod_eks/push.sh
+++ source/hyperpod_eks/push.sh
@@ -7,13 +7,14 @@ export REGISTRY=${ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com/
 echo "Logging in to $REGISTRY ..."
 aws ecr get-login-password | docker login --username AWS --password-stdin $REGISTRY
 
-# Create registry if it does not exist
-REGISTRY_COUNT=$(aws ecr describe-repositories | grep ${IMAGE} | wc -l)
+# Create ECR registry if it does not exist
+REGISTRY_COUNT=$(aws ecr describe-repositories | grep ${DOCKER_IMAGE_NAME} | wc -l)
 if [ "$REGISTRY_COUNT" == "0" ]; then
         echo ""
-        echo "Creating repository ${IMAGE} ..."
-        aws ecr create-repository --repository-name ${IMAGE}
+        echo "Creating ECR repository ${DOCKER_IMAGE_NAME} ..."
+        aws ecr create-repository --repository-name ${DOCKER_IMAGE_NAME}
 fi
 
 # Push image
+echo "pushing image: ${REGISTRY}${DOCKER_IMAGE_NAME}:aws"
 docker image push ${REGISTRY}${DOCKER_IMAGE_NAME}:aws
