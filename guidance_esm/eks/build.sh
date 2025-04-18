b3c90b3039a954f9fff8b43342c7e91614d51da3
Daniel Zilberman <42846514+dzilbermanvmw@users.noreply.github.com>
2025-04-17 21:51:54 -0700

    Update build.sh
    Updated image and tag

--- guidance_esm/eks/build.sh
+++ guidance_esm/eks/build.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker build -t ${DOCKER_IMAGE_NAME}:${TAG} .
+# docker build -t ${DOCKER_IMAGE_NAME}:${TAG} .
+export ACCOUNT=$(aws sts get-caller-identity --query Account --output text)
+export REGISTRY=${ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com/
+docker build -t ${REGISTRY}${DOCKER_IMAGE_NAME}${TAG} .
